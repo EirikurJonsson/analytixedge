d48b671bcf94bb9b891b5eeb40d8e0dea62fa375
EirikurJonsson <eirikur09@gmail.com>
2023-10-18 22:18:31 +0200

    layout will now only be in rows and columns - no more sidebar just topbar

--- src/app.py
+++ src/app.py
@@ -5,14 +5,8 @@ import dash_bootstrap_components as dbc  # pip install dash-bootstrap-components
 # Code from: https://github.com/plotly/dash-labs/tree/main/docs/demos/multi_page_example1
 
 app = dash.Dash(
-    __name__, external_stylesheets=[dbc.themes.SUPERHERO], use_pages=True
+    __name__, external_stylesheets=[dbc.themes.SLATE], use_pages=True
 )
-
-# for x in dash.page_registry.values():
-#     print(x)
-
-for i in dash.page_registry.values():
-    print(i)
 
 navbar = dbc.NavbarSimple(
     dbc.DropdownMenu(
@@ -24,18 +18,19 @@ navbar = dbc.NavbarSimple(
         nav=True,
         label="About",
     ),
-    brand="Multi Page App Plugin Demo",
-    color="light",
+    brand="",
+    color="secondary",
     dark=True,
-    className="mb-2",
+    className="mb-1",
 )
 SIDEBAR_STYLE = {
     "position": "fixed",
     "top": 0,
     "left": 0,
     "bottom": 0,
-    "width": "18rem",
+    "width": "15%",
     "padding": "2rem 1rem",
+    'height': '100%'
     #"background-color": "#f8f9fa",
 }
 
@@ -48,7 +43,12 @@ CONTENT_STYLE = {
 }
 sidebar = html.Div(
     [
-        html.H1("Analytix Edge", className="display-2", style = {'margin-top': '20px', 'offset': 20, 'text-align': 'center'}),
+        html.Div(
+            [
+                html.Img(src = './assets/images/android-chrome-192x192.png', style = {'margin-top': '10%', 'margin-left': '10%'}),
+                ]
+            ),
+        #html.H1("Analytix Edge", className="display-2", style = {'margin-top': '20px', 'offset': 20, 'text-align': 'center'}),
         html.Hr(),
         html.P(
             "Sharper Insights", className = 'lead display-8', style = {'text-align': 'center'}
@@ -69,6 +69,7 @@ sidebar = html.Div(
             pills=True,
         ),
     ],
+    className = 'sidebar',
     style=SIDEBAR_STYLE,
 )
 
--- src/pages/home.py
+++ src/pages/home.py
@@ -5,28 +5,47 @@ dash.register_page(__name__)
 from dash import Dash, dcc, html, Input, Output, callback
 import dash_bootstrap_components as dbc
 
-layout = dbc.Container(
-        [
-            dbc.Row(
-                dbc.Col(
-                    html.H1('Welcome To Analytix Edge', className='display-3'),
-                    width = {'size': 6, 'offset': 4}
-                    ),
-                ),
-            dbc.Row(
-                dbc.Col(
-                    html.P(
-                        """
-                        At Analytix Edge, we are passionate about the transformative potential of data science. Our mission is to empower businesses, organizations, and individuals with the analytical tools and insights they need to thrive in today's data-driven world. We specialize in an array of data-driven services that go beyond traditional data science, including web scraping services and dashboard building.
-                        """,
-                        className = 'lead',
-                        ),
-                    width = {
-                        'offset': 2
-                        }
-                    ),
-                )
-            ],
-        fluid = True,
-        )
 
+layout = dbc.Container([
+    dbc.Row(
+        dbc.Col(
+            html.H1("Welcome to Analytix Edge", className="display-3"),
+            width={"size": 6, "offset": 3}
+        ),
+    ),
+    dbc.Row(
+        dbc.Col(
+            html.Div([
+                html.P(
+                    """
+                    At Analytix Edge, we're at the forefront of data science and analytics, helping businesses unlock the hidden potential of their data. Our expertise lies in two key areas: machine learning and dashboard building.
+                    """,
+                    className="lead",
+                ),
+                html.P(
+                    """
+                    What sets us apart is our unique approach to data collection. We specialize in web scraping, a powerful technique that enables us to extract valuable information from the vast world of online data. This means you can eliminate blind spots and gain critical insights that traditional data sources might miss.
+                    """,
+                ),
+                html.P(
+                    """
+                    Here are a few examples of how Analytix Edge can benefit your business:
+                    """,
+                    className="lead",
+                ),
+                html.Ul([
+                    html.Li("Market Analysis: We can scrape competitor websites to gather pricing and product data for informed pricing strategies."),
+                    html.Li("Lead Generation: Our web scraping techniques can identify potential leads and contacts for your sales team."),
+                    html.Li("Competitive Intelligence: Track market trends, consumer sentiment, and competitor activities by collecting data from various sources."),
+                ]),
+            ],
+                style={
+                    "text-align": "justify",
+                    "margin-left": "20%",
+                    "margin-right": "5%",
+                }
+            ),
+            width={"size": 10}
+        ),
+    ),
+], fluid=True)
